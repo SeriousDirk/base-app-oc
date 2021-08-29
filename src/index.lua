98484e7215dca47c116b99d33bf0e0f1fa1c0c9c
Vlad Wild <theseriousdirk@gmail.com>
2021-08-29 17:06:21 +0300

    new panel

--- src/index.lua
+++ src/index.lua
@@ -1,30 +1,16 @@
--- Import this library
 local GUI = require("GUI")
--- We will also need downloaded double buffering library to render rectangles
-local buffer = require("doubleBuffering")
 
 --------------------------------------------------------------------------------
 
 -- Create new application
 local application = GUI.application()
 
--- Create and add template object to application
-local bar = application:addChild(GUI.object(3, 2, 10, 50))
--- Create own :draw() method and make it render green rectangle
-bar.draw = function(bar)
-  buffer.drawRectangle(bar.x, bar.y, bar.width, bar.height, 0x919191, 0x0, " ")
-end
+-- Add panel that fits application
+application:addChild(GUI.panel(1, 1, application.width, application.height, 0x262626))
+-- Add smaller red panel
+application:addChild(GUI.panel(1, (application.width - 20) / 2, application.width - 20, application.height - 40, 0x007a88))
 
-
-local light = bar:addChild(GUI.object(3, 2, 5, 5))
-
-light.draw = function(light)
-  buffer.drawRectangle(light.x, light.y, light.width, light.height, 0xf5e342, 0x0, " ")
-end 
 --------------------------------------------------------------------------------
 
--- Draw application content once on screen when program starts
 application:draw(true)
--- Start processing events for application
 application:start()
-
