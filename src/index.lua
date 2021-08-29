30d4e5b14ce10aac25a94921ead13afa3fa504f9
Vlad Wild <theseriousdirk@gmail.com>
2021-08-29 18:55:55 +0300

    new window "Light control"

--- src/index.lua
+++ src/index.lua
@@ -1,24 +1,60 @@
 local GUI = require("GUI")
+local component = require('component')
 
---------------------------------------------------------------------------------
-
+local windows = {
+  ["lightWindow"]: GUI.panel(10, 10, application.width - 20, application.height - 20, 0xf0dd0a),
+}
+local current = ""
 local application = GUI.application()
 application:addChild(GUI.panel(1, 1, application.width, application.height, 0x2D2D2D))
 
--- Add menu object to application
+
 local menu = application:addChild(GUI.menu(1, 1, application.width, 0xEEEEEE, 0x666666, 0x3366CC, 0xFFFFFF))
--- Add first item with black color. Attack a callback-function to it
+
+local actionButtonsRegular = menu:addChild(GUI.actionButtons(3, 2, false))
+
+actionButtonsRegular.close.onTouch = function()
+	application:stop()
+end
+
 menu:addItem("Base Control", 0x0).onTouch = function()
 	GUI.alert("Hello world!")
 end
 
-local actionButtonsRegular = application:addChild(GUI.actionButtons(application.width - 10, 2, false))
-
-actionButtonsRegular.close.onTouch = function()
-  application.close()
-end
--- Add context menu and few items to it
+local lightWindow
 local light = menu:addItem("Освещение")
+light.onTouch = function()
+	if current == "Освещение" then
+		return
+	end
+	showLightWindow()
+end
+
+
+function showLightWindow()
+	current = "Освещение"
+	lightWindow = application:addChild(windows["lightWindow"])
+	local switch = 1
+	for address, componentType in component.list("redstone") do
+		lightWindow:addChild(GUI.label(3, 2 + (2 * switch), 8, 5, 0xFFFFFF, tostring(switch)))
+		lightWindow:addChild(GUI.switch(5, 2+ (2 * switch), 8, 0x66DB80, 0x1D1D1D, 0xEEEEEE, false))
+	end
+end
+
+function componentRedstoneTable()
+	local redstone
+	local n = 1
+	for address, componentType in component.list() do 
+		if componentType == "redstone" then
+			while redstone[n] == nil do
+				n = n + 1
+			end;
+			redstone[n] = address
+		end
+	end
+	
+	return redstone
+end
 
 -- contextMenu:addItem("New")
 -- contextMenu:addItem("Open").onTouch = function()
@@ -31,8 +67,6 @@ local light = menu:addItem("Освещение")
 -- menu:addItem("Edit")
 -- menu:addItem("View")
 -- menu:addItem("About")
-
---------------------------------------------------------------------------------
 
 application:draw(true)
 application:start()
