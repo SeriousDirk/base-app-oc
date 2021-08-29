e6eedf12f543829c80757029cf54e72ae0b4f37f
Vlad Wild <theseriousdirk@gmail.com>
2021-08-29 17:24:05 +0300

    action Buttons

--- src/index.lua
+++ src/index.lua
@@ -8,22 +8,29 @@ application:addChild(GUI.panel(1, 1, application.width, application.height, 0x2D
 -- Add menu object to application
 local menu = application:addChild(GUI.menu(1, 1, application.width, 0xEEEEEE, 0x666666, 0x3366CC, 0xFFFFFF))
 -- Add first item with black color. Attack a callback-function to it
-menu:addItem("MineCode IDE", 0x0).onTouch = function()
+menu:addItem("Base Control", 0x0).onTouch = function()
 	GUI.alert("Hello world!")
 end
--- Add context menu and few items to it
-local contextMenu = menu:addContextMenu("File")
-contextMenu:addItem("New")
-contextMenu:addItem("Open").onTouch = function()
-	GUI.alert("Open item was pressed")
+
+local actionButtonsRegular = application:addChild(GUI.actionButtons(application.width - 10, 2, false))
+
+actionButtonsRegular.close.onTouch = function()
+  application.close()
 end
-contextMenu:addSeparator()
-contextMenu:addItem("Save")
-contextMenu:addItem("Save as")
--- Add whatever you want
-menu:addItem("Edit")
-menu:addItem("View")
-menu:addItem("About")
+-- Add context menu and few items to it
+local light = menu:addItem("Освещение")
+
+-- contextMenu:addItem("New")
+-- contextMenu:addItem("Open").onTouch = function()
+-- 	GUI.alert("Open item was pressed")
+-- end
+-- contextMenu:addSeparator()
+-- contextMenu:addItem("Save")
+-- contextMenu:addItem("Save as")
+-- -- Add whatever you want
+-- menu:addItem("Edit")
+-- menu:addItem("View")
+-- menu:addItem("About")
 
 --------------------------------------------------------------------------------
 
