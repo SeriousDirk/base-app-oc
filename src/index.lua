6ed9b264060af6bcebea5eaaeb6cf951519e75a8
Vlad Wild <theseriousdirk@gmail.com>
2021-08-29 19:09:53 +0300

    bug fix

--- src/index.lua
+++ src/index.lua
@@ -1,13 +1,10 @@
 local GUI = require("GUI")
 local component = require('component')
 
-local windows = {
-  ["lightWindow"]: GUI.panel(10, 10, application.width - 20, application.height - 20, 0xf0dd0a),
-}
+
 local current = ""
 local application = GUI.application()
 application:addChild(GUI.panel(1, 1, application.width, application.height, 0x2D2D2D))
-
 
 local menu = application:addChild(GUI.menu(1, 1, application.width, 0xEEEEEE, 0x666666, 0x3366CC, 0xFFFFFF))
 
@@ -33,28 +30,30 @@ end
 
 function showLightWindow()
 	current = "Освещение"
-	lightWindow = application:addChild(windows["lightWindow"])
+	lightWindow = GUI.addBackgroundContainer(application, true, true, "Освещение")
+	lightWindow:addChild(GUI.panel(10, 10, application.width - 20, application.height - 20, 0xf0dd0a))
+
 	local switch = 1
 	for address, componentType in component.list("redstone") do
-		lightWindow:addChild(GUI.label(3, 2 + (2 * switch), 8, 5, 0xFFFFFF, tostring(switch)))
-		lightWindow:addChild(GUI.switch(5, 2+ (2 * switch), 8, 0x66DB80, 0x1D1D1D, 0xEEEEEE, false))
+		lightWindow:addChild(GUI.switchAndLabel(5, 2 + (2 * switch), 8, 0x66DB80, 0x1D1D1D, 0xEEEEEE, false))
+		lightWindow:addChild(GUI.switchAndLabel(2, 2 + (2 * switch), 25, 8, 0x66DB80, 0x1D1D1D, 0xEEEEEE, 0x999999, tostring(switch), true))
 	end
 end
 
-function componentRedstoneTable()
-	local redstone
-	local n = 1
-	for address, componentType in component.list() do 
-		if componentType == "redstone" then
-			while redstone[n] == nil do
-				n = n + 1
-			end;
-			redstone[n] = address
-		end
-	end
+-- function componentRedstoneTable()
+-- 	local redstone
+-- 	local n = 1
+-- 	for address, componentType in component.list() do 
+-- 		if componentType == "redstone" then
+-- 			while redstone[n] == nil do
+-- 				n = n + 1
+-- 			end;
+-- 			redstone[n] = address
+-- 		end
+-- 	end
 	
-	return redstone
-end
+-- 	return redstone
+-- end
 
 -- contextMenu:addItem("New")
 -- contextMenu:addItem("Open").onTouch = function()
