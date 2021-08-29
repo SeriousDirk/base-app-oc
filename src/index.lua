c7710a27ab5abb83b5fef9b703e4eade665a9589
Vlad Wild <theseriousdirk@gmail.com>
2021-08-29 17:11:56 +0300

    menu

--- src/index.lua
+++ src/index.lua
@@ -2,13 +2,28 @@ local GUI = require("GUI")
 
 --------------------------------------------------------------------------------
 
--- Create new application
 local application = GUI.application()
+application:addChild(GUI.panel(1, 1, application.width, application.height, 0x2D2D2D))
 
--- Add panel that fits application
-application:addChild(GUI.panel(1, 1, application.width, application.height, 0x262626))
--- Add smaller red panel
-application:addChild(GUI.panel(1, (application.width - 20) / 2, application.width - 20, application.height - 40, 0x007a88))
+-- Add menu object to application
+local menu = application:addChild(GUI.menu(1, 1, application.width, 0xEEEEEE, 0x666666, 0x3366CC, 0xFFFFFF))
+-- Add first item with black color. Attack a callback-function to it
+menu:addItem("MineCode IDE", 0x0).onTouch = function()
+	GUI.alert("Hello world!")
+end
+-- Add context menu and few items to it
+local contextMenu = menu:addContextMenu("File")
+contextMenu:addItem("New")
+contextMenu:addItem("Open").onTouch = function()
+	GUI.alert("Open item was pressed")
+end
+contextMenu:addSeparator()
+contextMenu:addItem("Save")
+contextMenu:addItem("Save as")
+-- Add whatever you want
+menu:addItem("Edit")
+menu:addItem("View")
+menu:addItem("About")
 
 --------------------------------------------------------------------------------
 
